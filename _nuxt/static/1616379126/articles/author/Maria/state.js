ffafa15e13244a9bb7f57b8631e88cc3c88942ae
nathanfeitoza <nathanfeitoza@users.noreply.github.com>
2021-03-22 02:16:55 +0000

    deploy: 7e617422d74a1252891166abd986fd47fe0e75c2

--- _nuxt/static/1616379126/articles/author/Maria/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616379126",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:"\u002Farticles\u002Fauthor\u002FMaria",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f0600431"}}}}(null));
---
+++ _nuxt/static/1616379414/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616379414",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"04a454f9"}}}}(null,"\u002F"));
