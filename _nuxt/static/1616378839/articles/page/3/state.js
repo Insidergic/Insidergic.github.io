ab56c062feef1c3660ae98e3b006cd89d3d60a23
nathanfeitoza <nathanfeitoza@users.noreply.github.com>
2021-03-22 02:07:20 +0000

    deploy: 25896af74de93bf169027a49f44b1ff11039a426

---
+++ _nuxt/static/1616378839/articles/page/3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616378839",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:"\u002Farticles\u002Fpage\u002F3",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c28483c6"}}}}(null));
