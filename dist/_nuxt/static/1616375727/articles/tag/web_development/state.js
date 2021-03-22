7d78a805cad13684fec1312fba0c64427d8ec7dd
Nathan <nathanfeitoza@outlook.com.br>
2021-03-21 22:16:01 -0300

    ajustando

---
+++ dist/_nuxt/static/1616375727/articles/tag/web_development/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616375727",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:"\u002Farticles\u002Ftag\u002Fweb_development",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"37421725"}}}}(null));
---
+++ dist/_nuxt/static/1616375727/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616375727",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"37421725"}}}}(null,"\u002F"));
