53c8c4945c5b5a0df9aec2b769e2b34f8efccc17
nathanfeitoza <nathanfeitoza@users.noreply.github.com>
2021-03-22 02:19:51 +0000

    deploy: c02a65c4044da82e25d35a4a4bdd6e7960509e6d

--- _nuxt/static/1616379414/articles/tag/nuxtjs/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616379414",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:"\u002Farticles\u002Ftag\u002Fnuxtjs",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"04a454f9"}}}}(null));
---
+++ _nuxt/static/1616379588/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616379588",layout:"default",error:a,state:{articlesPerPage:3},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5e1d96cc"}}}}(null,"\u002F"));
